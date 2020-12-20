10252dff23b385d18d64793a6973376cbd83d90d
Roman Romanov <rroma.dev@gmail.com>
2020-12-20 14:57:03 +0300

    Add vue

---
+++ assets/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <h1>{{message}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  computed: {
+    message: () => {
+      return 'Hello'
+    }
+  }
+};
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var Encore = require('@symfony/webpack-encore');
+
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .addEntry('app', './assets/app.js')
+    .splitEntryChunks()
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    // enables hashed filenames (e.g. app.abc123.css)
+    .enableVersioning(Encore.isProduction())
+
+    .configureBabel((config) => {
+        config.plugins.push('@babel/plugin-proposal-class-properties');
+    })
+
+    // enables @babel/preset-env polyfills
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+   .enableVueLoader()
+;
+
+module.exports = Encore.getWebpackConfig();
